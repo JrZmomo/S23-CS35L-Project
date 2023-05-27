efd6f58bbfdd9f58e7fc820a469453cba3596d14
Leeland Zhang <66564492+leelandzhang@users.noreply.github.com>
2023-05-27 00:37:34 -0700

    Dynamics, Multi-clef, Bars, + bug fixes
    Continued to add dynamic features including the decrescendo, and text options (for anything like writing fortissimo, or other markings). Fixed bugs to allow for multi-clef, also fixed the rendering of crescendos to include on a stave that does not render, and subtract the y-positions that this invisible stave takes up for other staves. Attachment bars across notes less than a quarter note has also been added as a feature as part of the render function.

--- src/Backend/Sheet.js
+++ src/Backend/Sheet.js
@@ -9,8 +9,9 @@ class Note
 //pos must be an array of int, dur is a string, 
 //acc is accidentals, force is anything like accents
 
-  	constructor(pos=[0], dur="q", acc=null, force=null,rest=false)
+  	constructor(pos=[0], dur="q", rest=false, first=false, acc=null)
     {
+      this.first=first;
     	var collection=[]
       for(let i=0; i<pos.length; i++)
       {
@@ -61,11 +62,13 @@ class Note
       if(rest)
       	duration=duration.toString()+"r";
     	this.note=new Vex.Flow.StaveNote({ keys: collection, duration: duration });
+      //if(acc)//Cannot find note.addAccidental function, works online however...
+        //this.note.addAccidental(0, new Vex.Flow.Accidental(acc));
     }
     getNote()
-    {
-    	return this.note;
-    }
+    	{
+        return this.note;
+      }
 }
 
 class Marking
@@ -75,8 +78,26 @@ class Marking
   	constructor(dur="q",type)
     {
       this.mark;
+      var duration=0
+      for(let i=0; i<dur.length; i++)
+      {
+      	if(dur[i]=="q")
+        	duration=4;
+         else if(dur[i]=="w")
+         	duration=1;
+         else if(duration==0&&dur[i]=="h")
+         	duration=2;
+         else if(dur[i]=="h")
+         	duration*=2;
+      }
       if(type=="crescendo")
-        {this.mark=new Vex.Flow.Crescendo({duration: dur});}
+        {this.mark=new Vex.Flow.Crescendo({duration: duration});}
+      else if(type=="decrescendo")
+        {this.mark=new Vex.Flow.Crescendo({duration: duration}).setDecrescendo(true);}
+      else
+      {
+        this.mark= new Vex.Flow.TextDynamics({text: type,duration: duration});
+      }
     }
     getMarking()
     {
@@ -86,7 +107,7 @@ class Marking
 
 class Sheet 
 {
-	
+
 	constructor(clefnum=1, font="Times New Roman")
   {
   	this.SHEET_WIDTH=1220;
@@ -97,56 +118,77 @@ class Sheet
     this.ctx = this.renderer.getContext();
   	this.measures = [];
     this.noteset = [];
-    this.firstnote=true;
-    this.firstmeasure=true;
     var firstmeasure = new Vex.Flow.Stave(10, 3.14, 300);
-    var firstnotes=[new Note()];
+    var firstnotes=[new Note([0],'q',false,true)];
     for(let i = 0; i < clefnum; i++) 
     {
       this.measures.push([firstmeasure]);
       this.noteset.push([firstnotes]);
     }
-    
   }
-  render()
+  render(beams=null)
   {
-    
+    var beamcontain=[];
+    //first beams num specifying clef, second specifying measure, beyond are notes to be connected
+    if(beams)
+    for(let i = 0; i < beams.length; i++) 
+    {
+      var barr=[];
+      for(let j = 2; j < beams[i].length; j++) 
+      {
+        barr.push(this.noteset[beams[i][0]][beams[i][1]][beams[i][j]]);
+      }
+      const b = new Vex.Flow.Beam(barr);
+      beamcontain.push(b);
+      
+    }
   	for(let i = 0; i < this.clefnum; i++) 
     {
     	for(let j=0; j<this.measures[i].length; j++)
       {
       	this.measures[i][j].setContext(this.ctx).draw();
-      	Vex.Flow.Formatter.FormatAndDraw(this.ctx, this.measures[i][j],this.noteset[i][j],new Marking("q","crescendo"));
+      	Vex.Flow.Formatter.FormatAndDraw(this.ctx, this.measures[i][j],this.noteset[i][j]);
+      } 
+      if(i>0)
+      {
+        //brace is not working for an odd reason
+        //var brace = new Vex.Flow.StaveConnector(this.measures[0], this.measures[1]).setType(3);
+        //brace.setContext(this.ctx).draw();
+        //new Vex.Flow.StaveConnector(this.measures[i-1], this.measures[i]).setType('brace').setContext(this.ctx).draw();
       }
     }
+    
+    for(let i=0; i<beamcontain.length;i++)
+    {
+      beamcontain[i].setContext(this.ctx).draw();
+    }
   }
-  
   //Detecting and determining the position of a new measure. If first measure, must include a clef.
-  newMeasure(cleffnum=0,len=300,clef=null, keysig=null)
+  newMeasure(cleffnum=0,len=300,clef=null, keysig=null,timesig=null)
   {
   	var staveMeasure;
-    
+    var firstmeasure=false;
     if(this.measures[cleffnum][this.measures[cleffnum].length-1].x>this.SHEET_WIDTH-len)
-    	staveMeasure = new Vex.Flow.Stave(10, this.measures[cleffnum][this.measures[cleffnum].length-1].y+100*cleffnum, len);
-    else if(this.firstmeasure)
-      staveMeasure=new Vex.Flow.Stave(10, this.measures[cleffnum][this.measures[cleffnum].length-1].y, len);
+    	staveMeasure = new Vex.Flow.Stave(10, this.measures[cleffnum][this.measures[cleffnum].length-1].y
+        +100*Math.ceil(this.cleffnum/2), len);
+    else if(this.measures[cleffnum][0].y==3.14)
+      {staveMeasure=new Vex.Flow.Stave(10, Math.ceil(cleffnum/2)*100, len);firstmeasure=true;}
     else
-    	{staveMeasure = new Vex.Flow.Stave(this.measures[cleffnum][this.measures[cleffnum].length-1].x+len, this.measures[cleffnum][this.measures[cleffnum].length-1].y, len);}
+    	{staveMeasure = new Vex.Flow.Stave(this.measures[cleffnum][this.measures[cleffnum].length-1].x+
+        len, this.measures[cleffnum][this.measures[cleffnum].length-1].y, len);}
     
     if(keysig)
     {
-      
       staveMeasure.addKeySignature(keysig);
-      
     }
     if(clef)
       staveMeasure.addClef(clef);
-    	
-    if(this.firstmeasure)
+    if(timesig)
+      staveMeasure.addTimeSignature(timesig);
+    if(firstmeasure)
     {
       this.measures[cleffnum][0]=staveMeasure;
-      this.firstmeasure=false;
-      
+      firstmeasure=false;
     }
     else
       {this.measures[cleffnum].push(staveMeasure);}
@@ -157,6 +199,7 @@ class Sheet
 	newNoteSet(cleffnum=0,notes=null,markings=null)
   {
   	var notemeasure=[];
+    if(notes)
   	for(let i=0; i<notes.length; i++)
     { 
     	notemeasure.push(notes[i].getNote())
@@ -168,8 +211,8 @@ class Sheet
         notemeasure.push(markings[i].getMarking());
       }
     }
-    if(this.firstnote)
-      {this.noteset[cleffnum][0]=notemeasure;this.firstnote=false}
+    if(this.noteset[cleffnum][0][0].first)
+      {this.noteset[cleffnum][0]=notemeasure;}
     else 
       this.noteset[cleffnum].push(notemeasure);
   }
